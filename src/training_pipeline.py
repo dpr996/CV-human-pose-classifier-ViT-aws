bb3d1d7f01b53d33b3460a7bb10708ceb4b6cb03
Lahdhiri Moetez <moetez.lahdhiri@student-cs.fr>
2025-08-15 23:54:40 +0200

    fix pixel_values in batch dict

--- src/training_pipeline.py
+++ src/training_pipeline.py
@@ -56,7 +56,7 @@ class TrainingPipeline(BasePipeline):
 
         # Image preprocessing
         def apply_transforms(batch: dict) -> dict:
-            batch[BatchSchema.INPUTS] = [
+            batch[BatchSchema.PIXEL_VALUES] = [
                 _transforms(img.convert("RGB")) for img in batch["image"]
             ]
             del batch["image"]
@@ -76,7 +76,7 @@ class TrainingPipeline(BasePipeline):
             learning_rate=2e-5,
             per_device_train_batch_size=16,
             per_device_eval_batch_size=16,
-            num_train_epochs=5,
+            num_train_epochs=2,
             load_best_model_at_end=True,
             metric_for_best_model=MetricsSchema.ACCURACY,
             greater_is_better=True,
